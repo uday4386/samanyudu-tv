f07f3dc3fd2394f129a196349699290db8a38c03
swathi00487 <166385419+uday4386@users.noreply.github.com>
2026-02-26 14:05:57 +0530

    Update frontend APIs to use live Render URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import { NewsItem, ShortItem, NewsType } from '../types';
 
-const API_URL = 'http://172.16.25.13:5000/api';
+const API_URL = 'https://samanyudu-api.onrender.com/api';
 
 export const api = {
     // --- NEWS ---
